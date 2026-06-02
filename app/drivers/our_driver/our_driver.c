2a23444621b7ecea08b859580773590780995f3f
lime7git <e.bugajski7@gmail.com>
2026-06-02 20:12:06 +0200

    implement sensor driver controling LED

---
+++ app/drivers/our_driver/our_driver.c
@@ -0,0 +1,57 @@
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/drivers/gpio.h>
+
+#define DT_DRV_COMPAT our_driver
+
+/* The devicetree node identifier for the "led0" alias. */
+#define LED_NODE DT_ALIAS(led3)
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+
+LOG_MODULE_REGISTER(our_driver, LOG_LEVEL_INF);
+
+static int our_driver_init(const struct device *dev);
+static int our_driver_channel_get(const struct device *dev, enum sensor_channel channel, struct sensor_value* val);
+static int our_driver_sample_fetch(const struct device *dev, enum sensor_channel channel);
+
+static DEVICE_API(sensor, api_our_driver) = {
+    .sample_fetch = our_driver_sample_fetch,
+    .channel_get = our_driver_channel_get,
+};
+
+#define DEV_INST(inst) DEVICE_DT_INST_DEFINE(inst, our_driver_init, NULL, NULL, NULL, POST_KERNEL, 80, &api_our_driver);
+DT_INST_FOREACH_STATUS_OKAY(DEV_INST);
+
+static int our_driver_sample_fetch(const struct device *dev, enum sensor_channel channel)
+{
+    LOG_INF("Hello from our driver sample fetch %d", channel);
+    gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    LOG_INF("LED state: ON");
+    return 0;
+}
+
+static int our_driver_channel_get(const struct device *dev, enum sensor_channel channel, struct sensor_value* val)
+{
+    LOG_INF("Hello from our driver channel get %d", channel);
+    gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
+    LOG_INF("LED state: OFF");
+    return 0;
+}
+
+static int our_driver_init(const struct device *dev)
+{
+    LOG_INF("Hello from our driver init");
+    if (!gpio_is_ready_dt(&led))
+    {
+        LOG_ERR("LED not ready");
+        return -ENODEV;
+    }
+
+    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0)
+    {
+        LOG_ERR("Failed to configure LED");
+        return -ENODEV;
+    }
+    return 0;
+}
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,6 +1,7 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/sensor.h>
 
 #define SLEEP_TIME_MS 250
 
@@ -8,12 +9,14 @@
 #define LED_NODE DT_ALIAS(led0)
 #define LED_NODE1 DT_ALIAS(led1)
 #define LED_NODE2 DT_ALIAS(led2)
-#define LED_NODE3 DT_ALIAS(led3)
+//#define LED_NODE3 DT_ALIAS(led3)
+
+static const struct device * driver = DEVICE_DT_GET(DT_NODELABEL(our_driver0));
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED_NODE1, gpios);
 static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED_NODE2, gpios);
-static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(LED_NODE3, gpios);
+//static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(LED_NODE3, gpios);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
@@ -24,18 +27,21 @@ int main(void)
     if (!gpio_is_ready_dt(&led)) return 0;
     if (!gpio_is_ready_dt(&led1)) return 0;
     if (!gpio_is_ready_dt(&led2)) return 0;
-    if (!gpio_is_ready_dt(&led3)) return 0;
+    //if (!gpio_is_ready_dt(&led3)) return 0;
 
     if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
     if (gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE) < 0) return 0;
     if (gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE) < 0) return 0;
-    if (gpio_pin_configure_dt(&led3, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+    //if (gpio_pin_configure_dt(&led3, GPIO_OUTPUT_ACTIVE) < 0) return 0;
 
     while (1) {
         //if (gpio_pin_toggle_dt(&led) < 0) return 0;
         if (gpio_pin_toggle_dt(&led1) < 0) return 0;
-      //  if (gpio_pin_toggle_dt(&led2) < 0) return 0;
+        //if (gpio_pin_toggle_dt(&led2) < 0) return 0;
         //if (gpio_pin_toggle_dt(&led3) < 0) return 0;
+
+        if(led_state) sensor_channel_get(driver, SENSOR_CHAN_AMBIENT_TEMP, NULL);
+        else sensor_sample_fetch(driver);
 
         led_state = !led_state;
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
